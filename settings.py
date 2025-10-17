ebea183cd4c78dd6551ccddcabf3555af84e9579
Agastya Naik <agastya27naik@gmail.com>
2025-10-17 14:47:09 +0400

    settings_agastya_naik

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Class to hold settings for the application."""
+
+    def _init_(self):
+        """Initialize the application's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
