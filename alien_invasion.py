2b66292eb7ab7916a10f8d65cb070da03747a645
Agastya Naik <agastya27naik@gmail.com>
2025-10-18 16:55:14 +0400

    commit_5

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
             self.ship.update()
             self._update_bullets()
             self._update_screen()
+            self.clock.tick(60)
 
     def check_events(self):
         """Respond to keypresses and mouse events."""
@@ -59,8 +60,10 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        # Allow only 3 bullets at a time
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_bullets(self):
         """Update position of bullets and get rid of old bullets."""
@@ -78,9 +81,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         pygame.display.flip()
-        self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance and run the game.
     ai = AlienInvasion()
-    ai.run_game()
+    ai.run_game()           
--- settings.py
+++ settings.py
@@ -1,18 +1,16 @@
 class Settings:
-    """A class to store all settings for Alien Invasion."""
-    
     def __init__(self):
-        """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 600  # Smaller width
-        self.screen_height = 400  # Smaller height
+        self.screen_width = 600
+        self.screen_height = 400
         self.bg_color = (230, 230, 230)
-
-        # bullet settings
+        
+        # Ship settings
+        self.ship_speed = 1.5
+        
+        # Bullet settings
         self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        
-        # Ship settings
-        self.ship_speed = 1.5
+        self.bullets_allowed = 3
