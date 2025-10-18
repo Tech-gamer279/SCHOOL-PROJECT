93b5e1ed65e371e433dc1e9f477b98c6fbbdddbd
Agastya Naik <agastya27naik@gmail.com>
2025-10-18 16:31:29 +0400

    bullet_settings

--- settings.py
+++ settings.py
@@ -7,6 +7,12 @@ class Settings:
         self.screen_width = 600  # Smaller width
         self.screen_height = 400  # Smaller height
         self.bg_color = (230, 230, 230)
+
+        # bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
         
         # Ship settings
         self.ship_speed = 1.5
